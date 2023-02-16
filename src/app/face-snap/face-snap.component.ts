cfd41e1ace40995acf822a2c8c606948975e0c13
Camille Linan <camille.linan@hotmail.com>
2023-02-16 19:41:11 +0100

    snap card base OK

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+  title!: string;
+  description!: string;
+  createDate!: Date;
+  likes!: number;
+  imgUrl!: string;
+  buttonText!: string;
 
+  ngOnInit() {
+    this.title = 'Archibald';
+    this.description = 'Mon beau chien';
+    this.createDate = new Date();
+    this.likes = 6;
+    this.imgUrl = "../../assets/snapface_img1.jpg";
+    this.buttonText = 'Like it 😌'
+  }
+
+  onLike() {
+    if (this.buttonText === 'Like it 😌') {
+      this.likes++;
+      this.buttonText = 'Unlike it 😔';
+    } else {
+      this.likes--;
+      this.buttonText = 'Like it 😌';
+    }
+  }
 }

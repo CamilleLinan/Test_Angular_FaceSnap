6564adb9712cf3b9ce9500c6f1b560f1b6e17dc5
Camille Linan <camille.linan@hotmail.com>
2023-02-17 22:54:49 +0100

    add service

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FaceSnap } from '../models/face-snap.model';
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
+import { FaceSnapService } from '../services/face-snaps.service';
 
 @Component({
   selector: 'app-face-snap',
@@ -12,16 +13,18 @@ export class FaceSnapComponent implements OnInit {
   buttonText!: string;
   faLocationDot = faLocationDot;
 
+  constructor(private faceSnapService: FaceSnapService) {}
+
   ngOnInit() {
     this.buttonText = 'Like it 😌';
   }
 
   onLike() {
     if (this.buttonText === 'Like it 😌') {
-      this.faceSnap.likes++;
+      this.faceSnapService.likeFaceSnapById(this.faceSnap.id, 'like');
       this.buttonText = 'Unlike it 😔';
     } else {
-      this.faceSnap.likes--;
+      this.faceSnapService.likeFaceSnapById(this.faceSnap.id, 'dislike');
       this.buttonText = 'Like it 😌';
     }
   }

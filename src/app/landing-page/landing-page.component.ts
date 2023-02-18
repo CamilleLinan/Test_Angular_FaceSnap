ff8a7590749c69e9ca05bf5043d312691542546e
Camille Linan <camille.linan@hotmail.com>
2023-02-18 14:58:56 +0100

    basic form

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,6 +8,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
+  userName!: string;
+  userEmail!: string;
 
   constructor(private router: Router) {}
 
@@ -14,5 +17,9 @@ export class LandingPageComponent {
 
   onContinue(): void {
     this.router.navigateByUrl('facesnaps');
+  }
+
+  onSubmitForm(form: NgForm): void {
+    console.log(form.value);
   }
 }
